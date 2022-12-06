fed7f8712d644b71e99a5eb1ead9baf7a2415e78
Timo Koenig <timo@timokoenig.com>
2022-12-06 12:37:12 +0100

    Add jest tests

---
+++ src/cdk/utils/formatter.test.ts
@@ -0,0 +1,78 @@
+import { Operation } from "../classes/resource";
+import { formatChangesetTableRow, resourceNameFormatter } from "./formatter";
+import chalk = require("chalk");
+
+describe("formatChangesetTableRow", () => {
+  test("ADD", () => {
+    expect(
+      formatChangesetTableRow({
+        id: "1",
+        operation: Operation.ADD,
+        type: "type",
+        value_old: undefined,
+        value_new: undefined,
+      })
+    ).toStrictEqual([
+      chalk.green("+ add"),
+      chalk.green("1"),
+      chalk.green("type"),
+      "",
+      "",
+    ]);
+  });
+  test("MODIFY", () => {
+    expect(
+      formatChangesetTableRow({
+        id: "1",
+        operation: Operation.MODIFY,
+        type: "type",
+        value_old: "1",
+        value_new: "2",
+      })
+    ).toStrictEqual([
+      chalk.yellow("* modify"),
+      chalk.yellow("1"),
+      chalk.yellow("type"),
+      chalk.yellow("1"),
+      chalk.yellow("2"),
+    ]);
+  });
+  test("DELETE", () => {
+    expect(
+      formatChangesetTableRow({
+        id: "1",
+        operation: Operation.DELETE,
+        type: "type",
+        value_old: undefined,
+        value_new: undefined,
+      })
+    ).toStrictEqual([
+      chalk.red("- delete"),
+      chalk.red("1"),
+      chalk.red("type"),
+      "",
+      "",
+    ]);
+  });
+});
+
+describe("resourceNameFormatter", () => {
+  test("fails without namespace", () => {
+    const mockExit = jest
+      .spyOn(process, "exit")
+      .mockImplementation((number) => {
+        throw new Error("process.exit: " + number);
+      });
+    expect(() => resourceNameFormatter("", "foo")).toThrow();
+    expect(mockExit).toHaveBeenCalledWith(1);
+    mockExit.mockRestore();
+  });
+  test("format name without special characters", () => {
+    expect(resourceNameFormatter("space", "foo")).toBe("space-foo");
+  });
+  test("format name with special characters", () => {
+    expect(resourceNameFormatter("space", "foo-bar_world")).toBe(
+      "space-foo-bar-world"
+    );
+  });
+});
