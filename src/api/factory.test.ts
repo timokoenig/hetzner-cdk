66e9977621180ac1a4c2989b9878b3d631dc53e5
Timo Koenig <timo@timokoenig.com>
2022-12-18 21:06:47 +0100

    Add unit tests for APIFactory

---
+++ src/api/factory.test.ts
@@ -0,0 +1,30 @@
+import { CDKMock } from "../cdk/__mocks__/cdk";
+import { APIFactory, APIFactoryChangeset } from "./factory";
+import { FloatingIPAPI } from "./floatingip";
+import { FloatingIPAPIChangeset } from "./floatingip.changeset";
+import { PrimaryIPAPI } from "./primaryip";
+import { PrimaryIPAPIChangeset } from "./primaryip.changeset";
+import { ServerAPI } from "./server";
+import { ServerAPIChangeset } from "./server.changeset";
+import { SSHKeyAPI } from "./sshkey";
+import { SSHKeyAPIChangeset } from "./sshkey.changeset";
+
+describe("APIFactory", () => {
+  test("succeeds", async () => {
+    const sut = new APIFactory();
+    expect(sut.server).toBeInstanceOf(ServerAPI);
+    expect(sut.sshkey).toBeInstanceOf(SSHKeyAPI);
+    expect(sut.floatingip).toBeInstanceOf(FloatingIPAPI);
+    expect(sut.primaryip).toBeInstanceOf(PrimaryIPAPI);
+  });
+});
+
+describe("APIFactoryChangeset", () => {
+  test("succeeds", async () => {
+    const sut = new APIFactoryChangeset(new CDKMock());
+    expect(sut.server).toBeInstanceOf(ServerAPIChangeset);
+    expect(sut.sshkey).toBeInstanceOf(SSHKeyAPIChangeset);
+    expect(sut.floatingip).toBeInstanceOf(FloatingIPAPIChangeset);
+    expect(sut.primaryip).toBeInstanceOf(PrimaryIPAPIChangeset);
+  });
+});
