6bcf248a78f30ded1ede83e1fe2a9cec2c58f277
Timo Koenig <timo@timokoenig.com>
2022-12-25 09:32:25 +0100

    Disable sleep in unit tests

--- src/cdk/utils/sleep.ts
+++ src/cdk/utils/sleep.ts
@@ -1,4 +1,8 @@
 // Sleep for x seconds
 export function sleep(seconds: number) {
+  if (process.env.NODE_ENV === "test") {
+    // Do not sleep in unit tests
+    return Promise.resolve();
+  }
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
